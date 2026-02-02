f3f0282ded7b4cace5cf9c8e009d8ff83ee969ba
Hubblle <lubin.tsch@gmail.com>
2026-02-02 23:30:43 +0100

    Add differents level

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 let content = [];
-let max_mid = 0;
-let max_1 = 0;
+let max_mid = [];
+let max_1 = [];
 
 fetch("https://raw.githubusercontent.com/Hubblle/appreciation/refs/heads/main/data.json")
 .then(response => response.json())
 .then(data => {
     content = data;
-    max_mid = content["5"]["mid"].length;
-    max_1 = content["5"]["1"].length;
     console.log(content);
+    for(i=0; i<3; i++){
+        
+        max_mid[i] = content[i+1]["mid"].length;
+        max_1[i] = content[i+1]["1"].length;
+    }
+   
+    
 })
+// No catch, yes I'm like this x)
+
 
 
 function phrase(n) {
+    // get the level
+    level_input = document.getElementById("quantity").value;
+
+
     // Function which generate the phrases using the provided data, with n parts in second phrase
     let ph="";
     // Random phrase no1
-    let randint = Math.floor(Math.random() * max_1);
-    ph += content["5"]["1"][randint]+" ";
+    let randint = Math.floor(Math.random() * max_1[level_input-1]);
+    ph += content[level_input]["1"][randint]+" ";
 
     for(let i=0; i <= n; i++) {
 
         if(i++ == n){
-            randint = Math.floor(Math.random() * max_mid);
-            ph += content["5"]["mid"][randint]+".";
+            randint = Math.floor(Math.random() * max_mid[level_input-1]);
+            ph += content[level_input]["mid"][randint]+".";
             break;
         }
 
-        randint = Math.floor(Math.random() * max_mid);
-        ph += content["5"]["mid"][randint].charAt(0).toUpperCase()+content["5"]["mid"][randint].slice(1)+", ";
+        randint = Math.floor(Math.random() * max_mid[level_input-1]);
+        ph += content[level_input]["mid"][randint].charAt(0).toUpperCase()+content[level_input]["mid"][randint].slice(1)+", ";
 
     }
 
